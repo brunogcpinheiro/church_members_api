e38f6b00635e6372d8230ce31006a2d1d84fc3ed
Bruno Carpinelli <brunogcpinheiro@hotmail.com>
2019-03-03 11:55:49 -0300

    clean: remove unnecessary routes and imports

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,10 @@
 const express = require("express");
 const routes = express.Router();
-const UserController = require("./controllers/UserController");
-const SessionController = require("./controllers/SessionController");
-const authMiddleware = require("./middlewares/auth");
+//const authMiddleware = require("./middlewares/auth");
 
 const controllers = require("./controllers");
 
 routes.post("/users", controllers.UserController.store);
 routes.post("/sessions", controllers.SessionController.store);
-
-routes.get("/teste", authMiddleware, (req, res) => res.json({ ok: true }));
 
 module.exports = routes;
