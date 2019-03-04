178d1f058deb975065d86b24924d7833615aeb07
Bruno Carpinelli <brunogcpinheiro@hotmail.com>
2019-03-04 13:42:32 -0300

    fix: update server port

--- src/server.js
+++ src/server.js
@@ -15,4 +15,6 @@ mongoose.connect(databaseConfig.uri, {
 app.use(express.json());
 app.use(routes);
 
-app.listen(3000, () => console.log("Server is running on port 3000"));
+app.listen(3001 || process.env.PORT, () =>
+	console.log("Server is running on port 3001"),
+);
